d3f5312a729aa1268f5c6b7a9f2e134aa5b7489d
Jean <>
2023-08-18 11:17:08 +0200

    Refactored code to extract video_type and id from TMDB link in one variable. Optimized the code by removing unnecessary get_video_type function.

--- JellyHookAPI/jellyhookapi.py
+++ JellyHookAPI/jellyhookapi.py
@@ -11,12 +11,12 @@ import re
 TMDB_API_KEY = "TMDB_API_KEY"
 LANGUAGE = "fr-FR"
 LANGUAGE2 = "en-US"
-base_url = "https://api.themoviedb.org/3/"
+base_url = "https://api.themoviedb.org/3"
 
 app = Flask(__name__)
 
 # function to send to whatsapp API
-def send_whatsapp(title, overview, imdb, tmdb, idt, picture_path):
+def send_whatsapp(title, overview, imdb, tmdb, vidt, picture_path):
     # Parameters
     phone = "<phone-number>@s.whatsapp.net"
     # phone = "<group-number>@g.us" # group
@@ -37,7 +37,7 @@ def send_whatsapp(title, overview, imdb, tmdb, idt, picture_path):
     if overview:
         caption += f'```{overview}```\n'
     else:
-        caption += f'```{get_synopsis(idt)}```\n'
+        caption += f'```{get_synopsis(vidt)}```\n'
     #caption += f'_Watch here:{watch_link}_\n'
     # if imdb not empty, add it to the caption
     if imdb:
@@ -46,8 +46,8 @@ def send_whatsapp(title, overview, imdb, tmdb, idt, picture_path):
     if tmdb:
         caption += f'• TMDb: {shorten_link(tmdb)}\n'
     # get the shortened youtube trailer link from tmdb API
-    #trailer_links = get_trailer_link(idt)
-    caption += get_trailer_link(idt)
+    #trailer_links = get_trailer_link(vidt)
+    caption += get_trailer_link(vidt)
     # add the trailer link to the caption
     #caption += f'• Trailer FR: {trailer_links[0]}\n • Trailer EN: {trailer_links[1]}'
 
@@ -76,27 +76,8 @@ def send_whatsapp(title, overview, imdb, tmdb, idt, picture_path):
     # return the responses
     return response
 
-# function to get the type of the video from tmdb API (movie or tv show)
-def get_video_type(idt):
-    # parameters
-    global TMDB_API_KEY
-    global base_url
-
-    movie_url = f"{base_url}/movie/{idt}?api_key={TMDB_API_KEY}"
-    tv_url = f"{base_url}/tv/{idt}?api_key={TMDB_API_KEY}"
-    
-    response = requests.get(movie_url, timeout=10)
-    if response.status_code == 200:
-        return "movie"
-    
-    response = requests.get(tv_url, timeout=10)
-    if response.status_code == 200:
-        return "tv"
-    
-    return None
-
 # function to get the synopsis of the video from tmdb API
-def get_synopsis(idt):
+def get_synopsis(vidt):
     # parameters
     global TMDB_API_KEY
     global LANGUAGE
@@ -105,10 +86,9 @@ def get_synopsis(idt):
 
     languages = [LANGUAGE, LANGUAGE2]
 
-    video_type = get_video_type(idt)
     for language in languages:
-        if video_type:
-            url = f"{base_url}/{video_type}/{idt}?api_key={TMDB_API_KEY}&language={language}"
+        if vidt:
+            url = f"{base_url}/{vidt}?api_key={TMDB_API_KEY}&language={language}"
             response = requests.get(url, timeout=10)
             if response.status_code == 200:
                 return response.json()["overview"]
@@ -116,7 +96,7 @@ def get_synopsis(idt):
     return None # synopsis not found
 
 # function to get the shortened youtube trailer link from tmdb API
-def get_trailer_link(idt):
+def get_trailer_link(vidt):
     # parameters
     global TMDB_API_KEY
     global LANGUAGE
@@ -127,13 +107,10 @@ def get_trailer_link(idt):
     languages = [(LANGUAGE,r"bande[-\s]?annonce"), (LANGUAGE2,r"trailer")]
     trailer_links = []
 
-    # get the type of the video (movie or tv show)
-    video_type = get_video_type(idt)
-
     # search for the corresponding trailer depending on the language
     for language, pattern in languages:
-        if video_type:
-            youtube_key = search_trailer_key(idt, video_type, language, pattern)
+        if vidt:
+            youtube_key = search_trailer_key(vidt, language, pattern)
             if youtube_key:
                 trailer_links.append(f"https://youtu.be/{youtube_key}")
 
@@ -150,13 +127,13 @@ def get_trailer_link(idt):
     #return "Aucune bande-annonce trouvée" if not trailer_links else trailer_links
 
 # function to search for the trailer key in the tmdb API
-def search_trailer_key(idt, video_type, language, pattern):
+def search_trailer_key(vidt, language, pattern):
     # parameters
     global TMDB_API_KEY
     global base_url
 
     # search for the corresponding trailer depending on the language
-    url = f"{base_url}/{video_type}/{idt}/videos?api_key={TMDB_API_KEY}&language={language}"
+    url = f"{base_url}/{vidt}/videos?api_key={TMDB_API_KEY}&language={language}"
 
     response = requests.get(url, timeout=10)
 
@@ -175,15 +152,12 @@ def search_trailer_key(idt, video_type, language, pattern):
     return None # trailer key not found
 
 # function to get the poster url from tmdb API
-def get_tmdb_poster_url(idt):
+def get_tmdb_poster_url(vidt):
     global TMDB_API_KEY
     global base_url
 
-    # Get the video type.
-    video_type = get_video_type(idt)
-
     # poster url
-    tmdb_url = f"{base_url}/{video_type}/{idt}/images?api_key={TMDB_API_KEY}"
+    tmdb_url = f"{base_url}/{vidt}/images?api_key={TMDB_API_KEY}"
 
     # request to the TMDB API
     response = requests.get(tmdb_url, timeout=10)
@@ -232,19 +206,26 @@ def receive_data():
     picture_url = data.get('picture_url', '')
     server_name = data.get('server_name','')
 
-    # get tmdb id from the given tmdb link
-    # look for the id in the link
-    idt = tmdb.split("/")[-1]
+    # get tmdb video type, movie or tv, and id from the given tmdb link
+    # Split the link by "/" to extract the parts
+    parts = tmdb.split("/")
+    # Find the index of "tv" or "movie" in the parts list
+    type_index = parts.index("tv") if "tv" in parts else parts.index("movie")
+    # The type (tv or movie) will be the element after the type_index
+    video_type = parts[type_index]
+    # Extract the ID from the link
+    idt = parts[-1]
     # Remove extra text from ID, if any
     if "-" in idt:
         idt = idt.split("-")[0]
     # Check ID is numeric
     if not idt.isdigit():
         raise ValueError('The given TMDb link is not valid.')
+    vidt = f"{video_type}/{idt}"
 
     if picture_url == '':
         try:
-            picture_url = get_tmdb_poster_url(idt)
+            picture_url = get_tmdb_poster_url(vidt)
         except ValueError:
             # if the poster url is not found, use the default one
             picture_url = "./default-poster.png"
@@ -260,7 +241,7 @@ def receive_data():
         # DEBUG: print the path of the temporary file
         #print('picture_path: ', temp.name)
         # call the function to send to whatsapp API
-        send_whatsapp(title, overview, imdb, tmdb, idt, temp.name)
+        send_whatsapp(title, overview, imdb, tmdb, vidt, temp.name)
         # remove the temporary file (if: NamedTemporaryFile(delete=False))
         #os.remove(temp.name)
 
